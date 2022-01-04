68b532106340d993565facc7b6a9fec586d4c90e
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-01-04 22:13:39 +0800

    update

--- build_medicalgraph.py
+++ build_medicalgraph.py
@@ -14,7 +14,7 @@ class MedicalGraph:
         self.data_path = os.path.join(cur_dir, 'data/medical.json')
         self.g = Graph(
             host="127.0.0.1",  # neo4j 搭载服务器的ip地址，ifconfig可获取到
-            port=7474,  # neo4j 服务器监听的端口号
+            http_port=7474,  # neo4j 服务器监听的端口号
             user="neo4j",  # 数据库user name，如果没有更改过，应该是neo4j
             password="123456")
 
