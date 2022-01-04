8fd1db9307b2d57affbf27123bf7db1fc02ff43c
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-01-04 22:29:30 +0800

    update

--- answer_search.py
+++ answer_search.py
@@ -8,11 +8,7 @@ from py2neo import Graph
 
 class AnswerSearcher:
     def __init__(self):
-        self.g = Graph(
-            host="127.0.0.1",
-            http_port=7474,
-            user="neo4j",
-            password="123456")
+        self.g = Graph("bolt://localhost:7687", auth=("neo4j", "123456"))
         self.num_limit = 20
 
     '''执行cypher查询，并返回相应结果'''
